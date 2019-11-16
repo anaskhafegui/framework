a89f2a9ecdc4b575c93c97c73c47d7448442de9d
Mohamed Gamal <mg.dev1992@gmail.com>
2019-11-16 17:23:54 +0200

    [routing] add comment on router class methods

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -4,15 +4,31 @@ namespace Core\Router;
 
 class Router{
 
+    /**
+     * Routes List
+     *
+     * @var array
+     */
     private $routes = [];
 
-    private $prefix = [];
-
+    /**
+     * Get Routes List
+     *
+     * @return void
+     */
     public function list()
     {
         return $this->routes;
     }
 
+    /**
+     * Add a new route
+     *
+     * @param string $method
+     * @param string $uri
+     * @param string $action
+     * @return void
+     */
     public function add($method, $uri, $action)
     {
         $this->routes[] = [
@@ -21,20 +37,39 @@ class Router{
             'action' => $action,
             'name' => '',
         ];
-
-        return new static;
     }
 
+    /**
+     * Add a new GET route
+     *
+     * @param string $uri
+     * @param string $action
+     * @return void
+     */
     public function get($uri, $action)
     {
         $this->add('GET', $uri, $action);
     }
 
+    /**
+     * Add a new POST route
+     *
+     * @param string $uri
+     * @param string $action
+     * @return void
+     */
     public function post($uri, $action)
     {
         $this->add('POST', $uri, $action);
     }
 
+    /**
+     * Add a new GET|POST route
+     *
+     * @param string $uri
+     * @param string $action
+     * @return void
+     */
     public function any($uri, $action)
     {
         $this->add('POST', $uri, $action);
