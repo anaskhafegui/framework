40bbc24623b40d38d0989286c966ff4e2af5fc70
Mohamed Gamal <mg.dev1992@gmail.com>
2019-11-25 01:57:01 +0200

    [DB] init DB & QueryBuilder Interfaces

---
+++ core/src/Interfaces/QueryBuilderInterface.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace Core\Interfaces;
+
+interface QueryBuilderInterface
+{
+    public function table($table);
+
+    public function select();
+
+    public function join($table, $firstColumn, $secondColumn, $type = 'INNER');
+
+    public function rightJoin($table, $firstColumn, $secondColumn);
+
+    public function leftJoin($table, $firstColumn, $secondColumn);
+
+    public function where($column, $operator, $value);
+
+    public function groupBy();
+
+    public function having($column, $operator, $value);
+
+    public function orderBy($column, $type=null);
+
+    public function limit($limit);
+
+    public function offset($offset);
+
+    public function get();
+
+    public function first();
+
+    public function execute();
+
+    public function insert($data);
+
+    public function update($data);
+
+    public function delete();
+
+    public function paginate($itemPerPage);
+
+    public function links($currentPage, $pages);
+}
