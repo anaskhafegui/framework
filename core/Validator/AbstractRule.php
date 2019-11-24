ce3ae009ddaba838033fdfdbefbb89eaa095db49
Mohamed Gamal <mg.dev1992@gmail.com>
2019-11-24 13:26:00 +0200

    [validation] add docblock on methed apply()

--- core/Validator/AbstractRule.php
+++ core/Validator/AbstractRule.php
@@ -4,5 +4,12 @@ namespace Core\Validator;
 
 abstract class AbstractRule
 {
+    /**
+     * each child rule MUST extend and implement this function
+     *
+     * @param string $value
+     * @param string $parameter
+     * @return mixed
+     */
     public abstract function apply($value, $parameter);
 }
