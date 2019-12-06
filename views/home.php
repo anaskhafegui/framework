6e177fe57bbe73721c52381669ed29dd244671c3
Mohamed Gamal <mg.dev1992@gmail.com>
2019-12-06 09:42:29 +0200

    [views] change home page content

--- views/home.php
+++ views/home.php
@@ -1,3 +1,15 @@
 <p>
-Hello, <?php echo $name; ?>
+Users List
 </p>
+
+<ul>
+    <?php foreach($users as $user) { ?>
+    <li> 
+        <strong> 
+        <?php echo $user->id; ?>
+        </strong> : 
+        
+        <?php echo $user->name; ?>
+    </li>
+    <?php } ?>
+</ul>
