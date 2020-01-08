43c206310b5c80bf8e1a0bbb089250eab5dc4c1f
Mohamed Gamal <mg.dev1992@gmail.com>
2020-01-08 23:22:34 +0200

    [test] remove useles methods from application test class

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -32,45 +32,11 @@ final class ApplicationTest extends TestCase
     /**
      * @test
      */
-    public function instance()
-    {
-        $this->assertInstanceOf(Application::class, Application::instance());
-    }
-
-    /**
-     * @test
-     */
     public function instantiate()
     {
         $this->assertInstanceOf(FileSystem::class, $this->app->instantiate('file'));
         $this->assertInstanceOf(Hash::class, $this->app->instantiate('hash'));
         $this->assertInstanceOf(Request::class, $this->app->instantiate('request'));
         $this->assertInstanceOf(Response::class, $this->app->instantiate('response'));
-    }
-
-    /**
-     * @test
-     */
-    public function get()
-    {
-        $this->assertInstanceOf(Request::class, $this->app->get('request'));
-    }
-
-    /**
-     * @test
-     */
-    public function has()
-    {
-        $this->assertTrue($this->app->has('request'));
-        $this->assertFalse($this->app->has('not_found_class'));
-    }
-
-    /**
-     * @test
-     */
-    public function set()
-    {
-        $this->app->set('new_request', $this->app->instantiate('request'));
-        $this->assertTrue($this->app->has('new_request'));
     }
 }
