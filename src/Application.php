fec15c91ef483492ccf216cdaf02a575a356a72c
Mohamed Gamal <mg.dev1992@gmail.com>
2020-01-08 22:58:01 +0200

    [application] separate container methods from application class

--- src/Application.php
+++ src/Application.php
@@ -10,16 +10,9 @@ use Core\Http\Request;
 use Core\Http\Response;
 use Core\Router\Router;
 use Core\Session\Session;
-use Exception;
 
-class Application
+class Application extends Container
 {
-    /**
-     * Classes Container.
-     *
-     * @var array
-     */
-    private $container;
 
     /**
      * Singleton Instance.
@@ -33,7 +26,7 @@ class Application
      *
      * @var mixed
      */
-    private const CORE_CLASSES = [
+    protected const CORE_CLASSES = [
         'router'            => Router::class,
         'request'           => Request::class,
         'response'          => Response::class,
@@ -44,7 +37,7 @@ class Application
         'hash'              => Hash::class,
         'config'            => Config::class,
     ];
-
+    
     /**
      * Create a new application instance.
      *
@@ -66,91 +59,6 @@ class Application
         }
 
         return static::$instance;
-    }
-
-    /**
-     * Instantiate a class (Lazy Loading).
-     *
-     * @param string $key
-     *
-     * @return mixed
-     */
-    public function instantiate($key)
-    {
-        $object = isset(self::CORE_CLASSES[$key]) ? self::CORE_CLASSES[$key] : null;
-
-        return $object::instance();
-    }
-
-    /**
-     * If key not found, the magic method will be called.
-     *
-     * @param string $key
-     *
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        return $this->get($key);
-    }
-
-    /**
-     * Return the key from the container.
-     *
-     * @param string $key
-     *
-     * @return mixed
-     */
-    public function get($key)
-    {
-        $this->isKeyInCoreClasses($key);
-
-        if (!$this->has($key)) {
-            $this->set($key, $this->instantiate($key));
-        }
-
-        return $this->container[$key];
-    }
-
-    /**
-     * Check if the class exists in main core classes.
-     *
-     * @param string $key
-     *
-     * @return bool
-     */
-    public function isKeyInCoreClasses($key)
-    {
-        if (!isset(self::CORE_CLASSES[$key])) {
-            throw new Exception('Class Not Found');
-        }
-
-        return true;
-    }
-
-    /**
-     * Check if key exists in container.
-     *
-     * @param string $key
-     *
-     * @return bool
-     */
-    public function has($key)
-    {
-        return isset($this->container[$key]);
-    }
-
-    /**
-     * Set key in container.
-     *
-     * @param string $key
-     * @param string $value
-     *
-     * @return void
-     */
-    public function set($key, $value)
-    {
-        $this->container[$key] = $value;
     }
 
     /**
--- src/Container.php
+++ src/Container.php
@@ -6,4 +6,50 @@ use Psr\Container\ContainerInterface;
 
 class Container implements ContainerInterface
 {
+    protected $container = [];
+
+    public function get($key)
+    {
+        if(! $this->has($key)) {
+            $this->set($key, $this->instantiate($key));
+        }
+
+        return $this->container[$key];
+    }
+    
+    public function has($key)
+    {
+        return isset($this->container[$key]);
+    }
+
+    public function set($key, $value)
+    {
+        $this->container[$key] = $value;
+    }
+
+    /**
+     * If key not found, the magic method will be called.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Instantiate a class (Lazy Loading) from core classes.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function instantiate($key)
+    {
+        $object = Application::CORE_CLASSES[$key] ?? null;
+
+        return $object::instance();
+    }
 }
