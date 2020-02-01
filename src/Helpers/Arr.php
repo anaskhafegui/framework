c7edda50ed3e8a289c0fc7de993fdbb26207c5f7
Mohamed Gamal <mg.dev1992@gmail.com>
2020-02-02 01:56:40 +0200

    working on array helpers

---
+++ src/Helpers/Arr.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Core\Helpers;
+
+final class Arr
+{
+    public function add($array, $newElement = [])
+    {
+    }
+
+    public function except($array, $key = [])
+    {
+    }
+
+    public function firstWhen($array, callable $callback)
+    {
+    }
+
+    public function lastWhen($array, callable $callback)
+    {
+    }
+
+    public function flatten($multidimensionalArray)
+    {
+        // move it from helpers.php to here
+    }
+
+    public function forget($array, $key)
+    {
+    }
+
+    public function get($array, $key)
+    {
+    }
+
+    public function has($array, $key)
+    {
+    }
+
+    public function only($array, $slice = [])
+    {
+    }
+
+    public function pluck($array, $key)
+    {
+    }
+
+    public function set($array, $updatedElement = [])
+    {
+    }
+
+    public function where($array, callable $callable)
+    {
+    }
+}
