0bf220c25a21e5cda24f812ceb4f57c908c83385
Mohamed Gamal <mg.dev1992@gmail.com>
2020-02-01 13:57:24 +0200

    basic api controller

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -4,4 +4,69 @@ namespace Core\Controllers;
 
 abstract class ApiController extends Controller
 {
+    private $statusCode = 200;
+
+    private $message;
+
+    public function success($resource)
+    {
+        return [
+            'status_code'   => $this->getStatusCode(),
+            'data'          => $resource
+        ];
+    }
+
+    public function error($code, $message)
+    {
+        $this->setStatusCode($code)->setMessage($message);
+    }
+
+    public function notFound($message = 'Resource Not Found')
+    {
+        $this->error(404, $message);
+    }
+
+    public function forbidden($message = 'Forbidden')
+    {
+        $this->error(403, $message);
+    }
+
+    public function internalError($message = 'Internal Error')
+    {
+        $this->error(500, $message);
+    }
+
+    public function unauthorized($message = 'Unauthorized')
+    {
+        $this->error(401, $message);
+    }
+
+    public function wrongArgs($message = 'Wrong Arguments')
+    {
+        $this->error(400, $message);
+    }
+
+    private function setStatusCode($code):self
+    {
+        $this->statusCode = $code;
+        
+        return $this;
+    }
+
+    private function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
 }
