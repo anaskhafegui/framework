c30a38f421a161030548da73b6aa74f7db4ef1e1
Mohamed Gamal <mg.dev1992@gmail.com>
2020-03-01 17:23:44 +0200

    register commands in container

--- src/CLI/CommandHandler.php
+++ src/CLI/CommandHandler.php
@@ -2,19 +2,47 @@
 
 namespace Core\CLI;
 
-class CommandHandler
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class CommandHandler implements ContainerInterface
 {
     private $printer;
+
+    private $container;
 
     public function __construct(Printer $printer)
     {
         $this->printer = $printer;
     }
 
+
+    public function register($command, $callable)
+    {
+        $this->container[$command] = $callable;
+    }
+
+    public function get($command)
+    {
+        return $this->container[$command] ?? null;
+    }
+
+    public function has($command)
+    {
+        return isset($this->container[$command]);
+    }
+
     public function run(array $argv)
     {
-        $input = $argv[1] ?? "World";
-        $message = "Hello $input";
-        $this->printer->display($message);
+        $commandName = $argv[1] ?? "help";
+        $command = $this->get($commandName);
+
+        if (is_null($command)) {
+            echo "Not Found";
+            exit;
+        }
+
+        call_user_func($command, $argv);
     }
 }
